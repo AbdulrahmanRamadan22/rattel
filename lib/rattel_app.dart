5420d5a9e7715fdd88f7c2095e60b160297f810e
abdulrahman <abdulrahmanramadan22@gmail.com>
2026-01-30 20:19:51 +0200

    finish step app

--- lib/rattel_app.dart
+++ lib/rattel_app.dart
@@ -1,28 +1,34 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rattel/core/routing/on_generate_routes.dart';
 import 'package:rattel/core/routing/routes_name.dart';
-import 'package:rattel/core/theme/app_theme.dart';
+
+import 'core/theme/app_theme.dart';
 
 class RattelApp extends StatelessWidget {
   const RattelApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  ScreenUtilInit(
-  designSize: const Size(375, 812),
-  minTextAdapt: true,
-  splitScreenMode: true,
-  builder: (context, child) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Rattel',
-      // themeMode: ThemeMode.light,
-      // theme: AppTheme.lightTheme(context),
-      onGenerateRoute: onGenerateRoutes,
-      initialRoute: RoutesName.layoutScreen,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: "ورتل",
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme,
+          // darkTheme: AppTheme.darkTheme,
+          themeMode: ThemeMode.light,
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          onGenerateRoute: onGenerateRoutes,
+          initialRoute: RoutesName.layoutScreen,
+        );
+      },
     );
-  },
-);
   }
 }
